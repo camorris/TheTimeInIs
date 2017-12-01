bb4ba9225e640b95daedff232017448a5acf7c7c
Chase Morris <camorris@callutheran.edu>
2017-12-01 13:26:49 -0800

    updated css

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const ahourHand = document.querySelector('.a-hour-hand');
+const asecondHand = document.querySelector('.a-second-hand');
+const aminuteHand = document.querySelector('.a-min-hand');
+const bangkok = document.querySelector('[data-timeZone="Asia/Bangkok"]')
+const  dropDown = document.getElementById("myDropdown")
 
+
+bangkok.addEventListener('click',function(){
+  var bangkokClock = document.querySelector(".clock")
+  bangkokClock.style.opacity = '0';
+})
+function asiaZone(){
+  const asia = new moment().tz("Asia/Bangkok")
+  
+  const seconds = asia.seconds();
+  const secondsDegrees = ((seconds/60) * 360) + 90;
+  asecondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = asia.minutes();
+  const minsDegrees = ((mins/60) * 360) + ((seconds/60)*6)+90;
+  aminuteHand.style.form = `rotate(${minsDegrees}deg)`;
+
+  const hour = asia.hour();
+  const hourDegrees = ((hour/12)*360) + ((mins/60)*30) + 90 ;
+  ahourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  
+}
 
 
 function setDate(){
@@ -22,11 +49,12 @@ function setDate(){
 }
 setInterval(setDate, 1000)
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+setInterval(asiaZone, 1000)
 
-// Close the dropdown if the user clicks outside of it
+dropDown.addEventListener('mouseover', function() {
+dropDown.classList.toggle("show");
+})
+
 window.onclick = function(event) {
 if (!event.target.matches('.dropbtn')) {
 
